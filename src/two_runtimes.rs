50d04bd2000fb74344646664e0d1ee7b09e2353b
Jan Kaul <jankaul@mailbox.org>
2025-02-02 16:59:12 +0100

    use throttledstore

--- src/two_runtimes.rs
+++ src/two_runtimes.rs
@@ -5,13 +5,19 @@ use futures::{
     stream::{self, BoxStream},
     Stream, StreamExt, TryStreamExt,
 };
-use object_store::{aws::AmazonS3Builder, ObjectStore, PutPayload, Result};
+use object_store::{
+    aws::AmazonS3Builder,
+    throttle::{ThrottleConfig, ThrottledStore},
+    ObjectStore, PutPayload, Result,
+};
 
 mod dedicated_executor;
 mod localstack;
 
 static CPU_TIME: u64 = 2;
 static N_FILES: usize = 2;
+static MEGABYTE: usize = 1048576;
+static FILE_SIZE: usize = 8 * MEGABYTE;
 static OBJECT_KEY: &str = "test";
 static N_IO_THREADS: usize = 2;
 
@@ -24,7 +30,10 @@ async fn main() {
     let localstack_host = localstack.get_host().await.unwrap();
     let localstack_port = localstack.get_host_port_ipv4(4566).await.unwrap();
 
-    let object_store: Arc<dyn ObjectStore> = Arc::new(
+    let mut config: ThrottleConfig = Default::default();
+    config.wait_get_per_byte = Duration::from_micros(10);
+
+    let object_store: Arc<dyn ObjectStore> = Arc::new(ThrottledStore::new(
         AmazonS3Builder::new()
             .with_config("aws_access_key_id".parse().unwrap(), "user")
             .with_config("aws_secret_access_key".parse().unwrap(), "password")
@@ -37,14 +46,12 @@ async fn main() {
             .with_config("bucket".parse().unwrap(), "warehouse")
             .build()
             .unwrap(),
-    );
+        config,
+    ));
 
     // Insert object
     object_store
-        .put(
-            &OBJECT_KEY.into(),
-            PutPayload::from_static(&[0; 10 * 1024 * 1024]),
-        )
+        .put(&OBJECT_KEY.into(), PutPayload::from_static(&[0; FILE_SIZE]))
         .await
         .unwrap();
 
@@ -92,16 +99,15 @@ fn io_stream(
     object_store: Arc<dyn ObjectStore>,
 ) -> BoxStream<'static, Result<Vec<u8>, object_store::Error>> {
     Box::pin(
-        stream::iter(iter::repeat_n(object_store, N_FILES))
-            .then(|object_store| async move {
-                object_store
-                    .get(&OBJECT_KEY.into())
-                    .await
-                    .unwrap()
-                    .into_stream()
-                    .map_ok(|x| Vec::from(x))
-            })
-            .flatten(),
+        stream::iter(iter::repeat_n(object_store, N_FILES)).then(|object_store| async move {
+            object_store
+                .get(&OBJECT_KEY.into())
+                .await
+                .unwrap()
+                .bytes()
+                .await
+                .map(|x| Vec::from(x))
+        }),
     )
 }
 
